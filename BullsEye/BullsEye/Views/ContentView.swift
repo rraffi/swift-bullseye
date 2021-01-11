25de297da5f19b473ced4b38f82e728da0ed199f
Riaz Raffi <rraffi@pandora.com>
2021-01-11 09:09:51 -0500

    Project re-organized

--- BullsEye/BullsEye/Views/ContentView.swift
+++ BullsEye/BullsEye/Views/ContentView.swift
@@ -8,6 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State private var alertIsVisible: Bool = false
+    @State private var sliderValue: Double = 50.0
+    @State private var game: Game = Game()
+
     var body: some View {
         VStack {
             Text("🎯🎯🎯\nPUT THE BULLS EYE AS CLOSE AS YOU CAN")
@@ -16,7 +21,7 @@ struct ContentView: View {
                 .multilineTextAlignment(.center)
                 .lineSpacing(3.0)
                 .font(.footnote)
-            Text("89")
+            Text(String(game.target))
                 .kerning(-1.0)
                 .fontWeight(.black)
                 .lineSpacing(-1.0)
@@ -25,15 +30,29 @@ struct ContentView: View {
                 Text("1")
                     .bold()
                     .font(.headline)
-                Slider(value: .constant(50), in: 1.0 ... 100.0)
+                Slider(value: self.$sliderValue, in: 1.0 ... 100.0)
                     .padding(2)
                 Text("100")
                     .bold()
                     .font(.headline)
             }
-            Button(action: {}) {
+            Button(action: {
+                print("Clicked: Hit Me")
+                self.alertIsVisible = true
+            }) {
                 Text("Hit Me")
             }
+            .alert(isPresented: self.$alertIsVisible, content: {
+                let roundValue: Int = Int(self.sliderValue.rounded())
+                let scoreValue: Int = self.game.score(sliderValue: roundValue)
+                return  Alert(
+                    title: Text("Hello there!"),
+                    message: Text("Value hit is \(roundValue).\n" +
+                                    "You have scored \(scoreValue) points in this round."),
+                    dismissButton:
+                        .default(Text("Awesome"), action: { print("Value is \(self.sliderValue) | Score is \(scoreValue) | Clicked: Dismissed") })
+                )
+            })
         }
     }
 }
