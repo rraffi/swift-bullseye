380764a4fcc3d2ba79a2dde9a87052867986c8e3
Riaz Raffi <testifact@gmail.com>
2021-06-01 09:13:20 -0400

    Models and unit tests to start new round and restart game

--- BullsEye/BullsEyeTests/BullsEyeTests.swift
+++ BullsEye/BullsEyeTests/BullsEyeTests.swift
@@ -10,26 +10,52 @@ import XCTest
 
 class BullsEyeTests: XCTestCase {
 
-    var game: Game!
+  var game: Game!
 
-    override func setUpWithError() throws {
-        game = Game()
-    }
+  override func setUpWithError() throws {
+    game = Game()
+  }
 
-    override func tearDownWithError() throws {
-        game = nil
-    }
+  override func tearDownWithError() throws {
+    game = nil
+  }
 
-    func testScorePositive() throws {
-        let guess = game.target + 5
-        let score = game.score(sliderValue: guess)
-        XCTAssertEqual(score, 95)
-    }
+  func testScorePositive() throws {
+    let guess = game.target + 5
+    let score = game.points(sliderValue: guess)
+    XCTAssertEqual(score, 95)
+  }
 
-    func testScoreNegative() throws {
-        let guess = game.target - 5
-        let score = game.score(sliderValue: guess)
-        XCTAssertEqual(score, 95)
-    }
+  func testScoreNegative() throws {
+    let guess = game.target - 5
+    let score = game.points(sliderValue: guess)
+    XCTAssertEqual(score, 95)
+  }
 
+  func testScoreExact() throws {
+    let guess = game.target
+    let score = game.points(sliderValue: guess)
+    XCTAssertEqual(score, 200)
+  }
+
+  func testScoreClose() throws {
+    let guess = game.target + 2
+    let score = game.points(sliderValue: guess)
+    XCTAssertEqual(score, 98 + 50)
+  }
+
+  func testStartNewRound() throws {
+    game.startNewRound(points: 100)
+    XCTAssertEqual(game.score, 100)
+    XCTAssertEqual(game.round, 2)
+  }
+
+  func testRestart() throws {
+    game.startNewRound(points: 100)
+    XCTAssertNotEqual(game.score, 0)
+    XCTAssertNotEqual(game.round, 1)
+    game.restart()
+    XCTAssertEqual(game.score, 0)
+    XCTAssertEqual(game.round, 1)
+  }
 }
